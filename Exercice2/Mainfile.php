50737fc604eddc4adbf407c04f1f3c664688a438
Lililatortue <will.descoteaux@gmail.com>
2025-01-21 00:02:35 -0500

    quelque modification du deuxieme exercice

--- Exercice2/Mainfile.php
+++ Exercice2/Mainfile.php
@@ -1,3 +1,40 @@
+<?php
+//initialisation de l'annee
+$date_time = new DateTime('now');
+$year = $date_time -> format("Y");
+
+
+//nombre de jour dans un mois le (int) est une maniere de cast pour le foreach
+$jours_du_mois=(int)$date_time ->  format("t");
+
+
+//plus simple a formater surtout quand il faut changer de moi
+$month = $date_time -> format("n");     
+function whatMonthIsIt(int $x){
+    $month_map=[
+            0 => "jan",
+            1 => "feb", 
+            2 => "mar",
+            3 => "avr", 
+            4 => "mai",
+            5 => "jui", 
+            6 => "jul",
+            7 => "aout", 
+            8 => "sep",
+            9 => "oct",
+            10 => "nov", 
+            11 => "dec",   
+    ];
+            return $month_map[$x];
+    }
+
+    //maniere de savoir le 
+    $date = new DateTime($year . "-" . $month . "-01");
+    $dayoftheweek= (int)$date->format('N');
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,50 +43,37 @@
 </head>
 
 <body>
+    
     <a href="Mainfile.php?year=<?=($year-1)?>">avant</a>
-    <h2>annee</h2>
-    <a href="Mainfile.php?year=<?=($year+1)?>">apres </a>
+    <h2><?php echo $year; ?></h2>
+    <a href="Mainfile.php?year=<?=($year+1)?>">apres </a><br>
 
-    <a href="Mainfile.php?mois=<?=($mois+1)%12?>">avant</a>
-    <h2>mois</h2>
-    <a href="Mainfile.php?mois=<?=($mois+1)%12?>">apres </a>
+    <a href="Mainfile.php?mois=<?=($month-1)?>">avant</a>
+    <h2><?php echo whatMonthIsIt($month); ?></h2>
+    <a href="Mainfile.php?mois=<?=($month+1)?>">apres </a>
     <?php
-    echo "<table style=\"width:100%\">";
-    echo "<tr><th>Lundi</th> <th>Mardi</th> <th>Mercredie</th> <th>Jeudi</th> <th>Vendredi</th> <th>Samedi</th> <th>Dimanche</th></tr>";
-    for($days=0;$days<=35;){
-        echo"<tr>";
-        echo"<td>";
-        include "calendarday.php";
-        $days++;
-        echo"</td>";
-        echo"<td>";
-        include "calendarday.php";
-        $days++;
-        echo"</td>";
-        echo"<td>";
-        include "calendarday.php";
-        $days++;
-        echo"</td>";
-        echo"<td>";
-        include "calendarday.php";
-        $days++;
-        echo"</td>";
-        echo"<td>";
-        include "calendarday.php";
-        $days++;
-        echo"</td>";
-        echo"<td>";
-        include "calendarday.php";
-        $days++;
-        echo"</td>";
-        echo"<td>";
-        include "calendarday.php";
-        $days++;
-        echo"</td>";
-        echo"</tr>";
-             
-    }
-    echo "</table>";
+   echo $dayoftheweek;
+   echo "<table style=\"width:100%\">";
+   echo "<tr><th>Lundi</th> <th>Mardi</th> <th>Mercredi</th> <th>Jeudi</th> <th>Vendredi</th> <th>Samedi</th> <th>Dimanche</th></tr>";
+   //foreach pour chaque jours du mois
+  echo "<tr>";
+   for($days=1;$days<=$jours_du_mois+$dayoftheweek;$days++){
+       
+       //les deux if statement permetent de que se soit reflette
+
+       echo"<td>";
+       //si le jours de la semaine est = ou non a days ne include pas le script
+       if ($dayoftheweek<=$days){
+       include "calendarday.php";
+       }
+
+       echo"</td>";
+
+       if($days%7==0){echo "</tr><tr>";}
+   
+   }
+   echo "</tr>";
+   echo "</table>";
 
 
     ?>
